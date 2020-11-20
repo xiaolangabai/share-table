2326b74ba98ac6f84d677383e616125034fef69a
zhousihao <zhousihao>
2020-11-20 14:05:23 +0800

    commit

---
+++ src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.example.controller;
+
+import com.example.entity.User;
+import com.example.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RequestMapping("user")
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @RequestMapping("save")
+    public String save() {
+        for (int i = 0; i <10 ; i++) {
+            User user=new User();
+            user.setName("test"+i);
+            user.setCityId(1%2==0?1:2);
+            user.setCreateTime(new Date());
+            user.setSex(i%2==0?1:2);
+            user.setPhone("11111111"+i);
+            user.setEmail("xxxxx");
+            user.setCreateTime(new Date());
+            user.setPassword("eeeeeeeeeeee");
+            userMapper.save(user);
+        }
+        return "success";
+    }
+
+    @RequestMapping("get/{id}")
+    public User get(@PathVariable Long id) {
+        User user =  userMapper.get(id);
+        System.out.println(user.getId());
+        return user;
+    }
+
+}
---
+++ src/main/java/com/example/entity/AddressMapper.java
@@ -0,0 +1,16 @@
+//package com.example.entity;
+//
+//public interface AddressMapper {
+//
+//    /**
+//     * 保存
+//     */
+//    void save(Address address);
+//
+//    /**
+//     * 查询
+//     * @param id
+//     * @return
+//     */
+//    Address get(Long id);
+//}
